79bd10dbda440310095089471250406d390cc532
Jargaltogtokh <Jagaa155@gmail.com>
2024-12-20 18:00:50 +0800

    updated

--- src/components/StepThree.jsx
+++ src/components/StepThree.jsx
@@ -28,7 +28,7 @@ export const StepThree = ({
     <div className="w-full h-screen flex justify-center bg-[#F4F4F4]">
       <div className="w-[480px] h-[655px] border-2 bg-white mt-[182px]">
         <div className="m-[32px]">
-          <img src="Main1.png" width={60} height={60} alt="" />
+          <img src="Main1.png" width={100} height={100} alt="" />
           <p className="text-2xl font-bold"> Join Us! 😎 </p>
           <p className="text-gray-500 mb-[10px]">
             {" "}
